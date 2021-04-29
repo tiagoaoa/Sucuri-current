519210d71a20fc051d4fbec9f880ee544efda304
Tiago Alves <tiagoaoa@gmail.com>
2021-04-29 02:35:07 -0300

    Including subclass SchedulerWS

--- wservice/webservice.py
+++ wservice/webservice.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from multiprocessing import Process, Queue
+
 sys.path.append(os.environ['PYDFHOME'])
 
 from socketserver import ThreadingMixIn
@@ -15,20 +16,38 @@ class Iter_Queue():
         self.q = Queue()
 
     def put(self, x):
-        print("Enqueueing {}".format(x))
+        print("Iter Queue Enqueueing {} -- {}".format(x, self))
         self.q.put(x)
 
     def __iter__(self):
-            yield self.q.get()
+        return self
+    def __next__(self):
+        return self.q.get()
+
+iter_q = Iter_Queue()
+#for i in range(10):
+#    q.put(i)
+#for x in q:
+#    print("Dequeued {}".format(x))
 
 
 
-ResponseQueues = {}
+class SchedulerWS(Scheduler):
+    def all_idle(self, workers):
+        return False
+
+
+class NodeDebug(Node):
+    def __repr__(self):
+        return "Node for Debugging {}".format(id(self))
+
+ResponseQueues = {777: "Teste"}
 
 class SourceWS(Source):
     def f(self, line, args):
-        print("Receiving request {} {}".format(line, self.tagcounter))
-        ResponseQueues[self.tagcounter] = Queue()
+        print("[SourceWS] Receiving request {} {}".format(line, self.tagcounter))
+        #ResponseQueues[self.tagcounter] = Queue()
+        print("[SourceWS] Waiting... {}".format(ResponseQueues))
         return line
     
 
@@ -37,9 +56,9 @@ class ThreadedXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
 
 
 class WebService(Process):
-    def __init__(self, server):
+    def __init__(self, server, resp_q):
         Process.__init__(self)
-
+        self.resp_q = resp_q
 
 
         server = ThreadedXMLRPCServer(server)
@@ -48,16 +67,19 @@ class WebService(Process):
 
         self.server = server
         self.req_queue = Iter_Queue()
+        self.node = SourceWS(self.req_queue)
+
 
         def service(args):
-            resp_q = ResponseQueues
-            print("Args in service {}".format(args))
+            print("Args in service {} queue {} {}".format(args, ResponseQueues, self.req_queue))
             
             self.req_queue.put(args)
-            rq = ResponseQueues.pop(args.tag)
-            
-            print("Queues {}".format(ResponseQueues))
-            return rq.get()
+
+            rq = self.resp_q
+            #rq = ResponseQueues[args[0].tag]
+            print("[Service] Queues {}".format(self.resp_q))
+            #return rq.get()
+            return 7777
 
 
         server.register_function(service)
@@ -80,22 +102,19 @@ def response_enqueue(args):
 
 
 
-
 nprocs = int(sys.argv[1])
 
 graph = DFGraph()
-sched = Scheduler(graph, nprocs, mpi_enabled = False)#, wservice = ("localhost", 8000))
+sched = SchedulerWS(graph, nprocs, mpi_enabled = False)#, wservice = ("localhost", 8000))
 
 
-wservice = WebService(("localhost", 8000))
+wservice = WebService(("localhost", 8000), ResponseQueues)
 wservice.start()
 
-req_iter = wservice.req_queue
 
-
-reqs = SourceWS(req_iter)
-
-response = Node(response_enqueue, 1)
+#reqs = SourceWS(iter_q)
+reqs = wservice.node
+response = NodeDebug(response_enqueue, 1)
 
 
 graph.add(reqs)
@@ -104,7 +123,8 @@ graph.add(response)
 
 reqs.add_edge(response, 0)
 
-
+reqs.pin([0])
+response.pin([1])
 
 sched.start()
 
