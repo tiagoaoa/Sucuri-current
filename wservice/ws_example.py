1dea63b74225c1ef38de68a56a8eddea294bad7b
Tiago Alves <tiagoaoa@gmail.com>
2021-05-30 19:39:30 -0300

    First testable commit with practical example for web service

--- wservice/ws_example.py
+++ wservice/ws_example.py
@@ -12,10 +12,25 @@ sched = SchedulerWS(graph, nprocs, mpi_enabled = False)
 req_node, resp_node = sched.set_wservice(("localhost", 8000))
 
 
+def filter_function(args):
+    return args[0][0]+1
+
+
+
+
+
+filter_node = FilterTagged(filter_function,1)
+
 graph.add(req_node)
+
+graph.add(filter_node)
+
 graph.add(resp_node)
 
-req_node.add_edge(resp_node, 0)
+req_node.add_edge(filter_node, 0)
+filter_node.add_edge(resp_node,0)
+
+
 
 
 sched.start()
